7a54673043982d6b3757a34fe0023e3935c86c24
Mario Jorquera <mariojz@gmail.com>
2016-10-31 21:41:30 -0300

    feat: restaurant detail page

--- js/restaurant-app.js
+++ js/restaurant-app.js
@@ -1,6 +1,10 @@
 (function() {
   var app = angular.module('reviewer', []);
 
+  app.config(['$locationProvider', function($locationProvider){
+    $locationProvider.html5Mode(true);
+  }]);
+
   app.controller('ReviewsController', function($scope, $http){
     $http.get('data/restaurants.json').then(function(response) {
         $scope.restaurants = response.data.restaurants;
@@ -11,7 +15,6 @@
             restaurant.reviewCount = 0;
           });
         });
-        debugger;
     });
 
     $scope.getAverage = function(idRestaurant){
@@ -22,6 +25,32 @@
 
     var countReviews = function(idRestaurant){
 
+    };
+  });
+
+  app.controller('ReviewController', function($scope, $http, $location){
+    var id = $location.search().id;
+    $http.get('data/restaurants.json').then(function(response) {
+        var result = $.grep(response.data.restaurants, function(e) {return e.id ==id; });
+        $scope.restaurant = result[0];
+        //get the review for the restaurant
+        $http.get('data/reviews/' + id + '.json').then(function(response) {
+          $scope.restaurant.reviewCount = response.data.reviews.length;
+          $scope.restaurant.reviews = response.data.reviews;
+          $scope.restaurant.reviewRating = getAverage(response.data.reviews);
+        }, function(response){
+          $scope.restaurant.reviewCount = 0;
+        });
+    });
+
+    var getAverage = function(reviews){
+      var length = reviews.length;
+      var sum = 0;
+      reviews.forEach(function(review){
+        sum += review.rating;
+      });
+      var average = sum / length;
+      return average.toFixed(1);
     };
   });
 
